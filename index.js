85a7d33bc82423e95524cd6ea3b2a661acca7596
SaraAzmin <azmin.sara17@gmail.com>
2022-05-06 00:03:09 +0200

    product deletion implemented

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ async function run() {
             res.send(result);
         })
 
+        //delete product by id
+        app.delete('/groceries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await groceryCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
