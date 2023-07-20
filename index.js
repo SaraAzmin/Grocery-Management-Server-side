28fc18224ca0e99d247d9990f3485908e10ee38d
SaraAzmin <azmin.sara17@gmail.com>
2023-07-20 20:40:38 +0200

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,83 +10,86 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gw9bu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
+  try {
+    client.connect();
+    const groceryCollection = client
+      .db("groceryInventory")
+      .collection("groceries");
 
-    try {
-        await client.connect();
-        const groceryCollection = client.db('groceryInventory').collection('groceries');
+    //load all grocery items
+    app.get("/groceries", async (req, res) => {
+      const query = {};
+      const cursor = groceryCollection.find(query);
+      const groceries = await cursor.toArray();
+      res.send(groceries);
+    });
 
-        //load all grocery items
-        app.get('/groceries', async (req, res) => {
-            const query = {};
-            const cursor = groceryCollection.find(query);
-            const groceries = await cursor.toArray();
-            res.send(groceries);
-        });
+    //load item by id
+    app.get("/groceries/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const grocery = await groceryCollection.findOne(query);
+      res.send(grocery);
+    });
 
-        //load item by id
-        app.get('/groceries/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const grocery = await groceryCollection.findOne(query);
-            res.send(grocery);
-        });
+    //add product
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await groceryCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
-        //add product
-        app.post('/product', async (req, res) => {
+    //delete product by id
+    app.delete("/groceries/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await groceryCollection.deleteOne(query);
+      res.send(result);
+    });
 
-            const newProduct = req.body;
-            const result = await groceryCollection.insertOne(newProduct);
-            res.send(result);
-        })
+    //update quantity
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
 
-        //delete product by id
-        app.delete('/groceries/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await groceryCollection.deleteOne(query);
-            res.send(result);
-        })
+      const result = await groceryCollection.findOne({
+        _id: ObjectId(id),
+      });
 
-        //update quantity 
-        app.put('/inventory/:id', async (req, res) => {
-            const id = req.params.id;
+      const updatedProduct = req.body;
 
-            const result = await groceryCollection.findOne({
-                _id: ObjectId(id)
-            });
+      let quantity = updatedProduct.newQuantity;
 
-            const updatedProduct = req.body;
-
-            let quantity = updatedProduct.newQuantity;
-
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    quantity: quantity
-                }
-            };
-            const result2 = await groceryCollection.updateOne(filter, updatedDoc, options);
-            res.send(result2);
-
-        })
-    }
-
-    finally {
-
-    }
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: quantity,
+        },
+      };
+      const result2 = await groceryCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result2);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Running grocery server');
+app.get("/", (req, res) => {
+  res.send("Running grocery server");
 });
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
-})
+  console.log("Listening to port", port);
+});
